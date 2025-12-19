8f4d8091a72ef2f8101301e8cc72468c0f19b82f
EC2 Default User <ec2-user@ip-172-31-29-45.ec2.internal>
2025-12-19 12:52:28 +0000

    changed stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-isExistApp="$(pgrep httpd)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop httpd.service
-fi
-isExistApp="$(pgrep tomcat)"
-if [[ -n $isExistApp ]]; then
-sudo systemctl stop tomcat.service
-fi
+set -e
 
+APP_NAME=nextwork-web-project
+TOMCAT_WEBAPPS=/usr/share/tomcat/webapps
+
+echo "Stopping services if running..."
+
+sudo systemctl stop httpd || true
+sudo systemctl stop tomcat || true
+
+echo "Removing old Tomcat deployment..."
+
+sudo rm -f  $TOMCAT_WEBAPPS/$APP_NAME.war
+sudo rm -rf $TOMCAT_WEBAPPS/$APP_NAME
+
+echo "ApplicationStop completed successfully."
